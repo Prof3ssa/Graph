2f82e0aa4e9e0fc76bdcbc1450bdac05fb7cdca2
Michael Kitange <kalgecin@gmail.com>
2013-02-25 13:16:36 +0400

    bits of changes

--- Graph/Graph/Graph.h
+++ Graph/Graph/Graph.h
@@ -6,5 +6,7 @@ class Graph{
 	edge *edges;
 public:
 	Graph(void);
+	int addNode();			//adds a node to nodes and returns the position of the new node
+	void delNode(int i);	//deletes a node i from nodes. has to cleanup the edges too and other stuff related
 	~Graph(void);
 };
--- README.txt
+++ README.txt
@@ -1,1 +1,1 @@
-This is a try to create a class for graph functions.
+This is a try to create a class for graph functions using Visual Studio.
