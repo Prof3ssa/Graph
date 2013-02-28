bb38b1c2dbd48a47def1078478eb684a938a81f1
Michael Kitange <kalgecin@gmail.com>
2013-02-28 06:50:48 +0400

    added test of distance

--- Graph/Graph/main.cpp
+++ Graph/Graph/main.cpp
@@ -16,6 +16,11 @@ int main(){
 	g.addEdge(3,2);
 	g.addEdge(1,5);
 	cout<<"2 and 3 are "<<(g.isConnected(2,3)?"connected":"not connected")<<endl;
+	try{
+		cout<<"distance 0 and 2 is "<<g.distance(0,2)<<endl;
+	}catch(int i){
+		cout<<"Cought "<<i<<endl;
+	}
 	g.print();
 	return 0;
 }
